1ca45a9da10fa953375e10e742a36c2fcfd9cf63
Quinten <quinten.roets@gmail.com>
2024-06-11 04:47:30 +0000

    remove unused code (#25)

--- src/webapp_starter/models/path.py
+++ src/webapp_starter/models/path.py
@@ -1,5 +1,4 @@
-import typing
-from typing import TypeVar
+from typing import TypeVar, cast
 
 import superpathlib
 from simple_classproperty import classproperty
@@ -17,4 +16,10 @@ class Path(superpathlib.Path):
     @classproperty
     def assets(cls: type[T]) -> T:
         path = cls.script_assets / cls.source_root.name
-        return typing.cast(T, path)
+        return cast(T, path)
+
+    @classmethod
+    @classproperty
+    def config(cls: type[T]) -> T:
+        path = cls.assets / "config" / "config.yaml"
+        return cast(T, path)
