2fae3f62547baa95620780dca69b387c0db3cccd
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2025-01-07 21:12:43 -0800

    resolve conflicts

--- src/webapp_starter/models/secrets_.py
+++ src/webapp_starter/models/secrets_.py
@@ -3,13 +3,5 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-<<<<<<< HEAD
-=======
-class ApiSecrets:
-    token: str = field(default_factory=lambda: os.environ.get("API_TOKEN", ""))
-
-
-@dataclass
->>>>>>> template
 class Secrets:
     github_token: str
