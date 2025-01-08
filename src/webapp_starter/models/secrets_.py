f5d57ffb4b0dd12423ac001e10f70ecdadc2b3c2
Quinten <quinten.roets@gmail.com>
2025-01-08 05:10:15 +0000

    pre-commit auto updates

--- src/webapp_starter/models/secrets_.py
+++ src/webapp_starter/models/secrets_.py
@@ -1,6 +1,15 @@
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field
 
 
 @dataclass
+<<<<<<< HEAD
+=======
+class ApiSecrets:
+    token: str = field(default_factory=lambda: os.environ.get("API_TOKEN", ""))
+
+
+@dataclass
+>>>>>>> template
 class Secrets:
     github_token: str
